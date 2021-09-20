23711f6582f67ede2aed03612567e06205113de3
guzman-raphael <raphael.h.guzman@gmail.com>
2021-09-20 03:37:32 -0500

    Update size limit.

--- tests/main.sh
+++ tests/main.sh
@@ -62,10 +62,9 @@ elif [ $DISTRO == debian ] && [ $PY_VER == '3.7' ] && [ $PLATFORM == 'linux/amd6
 elif [ $DISTRO == debian ] && [ $PY_VER == '3.6' ] && [ $PLATFORM == 'linux/amd64' ]; then
 	SIZE_LIMIT=496  # 923
 fi
-SIZE_LIMIT=$(echo "scale=4; $SIZE_LIMIT * 1.03" | bc)
+SIZE_LIMIT=$(echo "scale=4; $SIZE_LIMIT * 1.06" | bc)
 # verify size minimal
 SIZE=$(docker images --filter "reference=$REF" --format "{{.Size}}" | awk -F'MB' '{print $1}')
-echo $SIZE
 assert "minimal footprint" "(( $(echo "$SIZE <= $SIZE_LIMIT" | bc -l) ))" $LINENO
 # run tests
 SHELL_CMD=$(eval "echo \"$SHELL_CMD_TEMPLATE\"")
